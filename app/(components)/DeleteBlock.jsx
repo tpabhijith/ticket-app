ff907f83fb1807e319f6df3ba791516139c6097b
abhijithtp <abhijith.tp@factweavers.com>
2023-11-02 23:22:46 +0530

    new changes

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -8,9 +8,12 @@ const DeleteBlock = ({ id }) => {
     const router = useRouter();
 
     const deleteTicket = async () => {
-        const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-            method: "DELETE",
-        });
+        const res = await fetch(
+            `http://ticket-app-liard.vercel.app/api/Tickets/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
         console.log(res, "response");
         if (res.ok) {
             router.refresh();
--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,9 +1,12 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
 const getTicketById = async (id) => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-        cache: "no-store",
-    });
+    const res = await fetch(
+        `http://ticket-app-liard.vercel.app/api/Tickets/${id}`,
+        {
+            cache: "no-store",
+        }
+    );
     if (!res.ok) {
         throw new Error("Failed to get Ticket");
     }
